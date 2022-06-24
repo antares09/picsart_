ee86dbed7c50632f15a4ff79cf6a4a4c2930018a
antares <erkanyananna@mail.ru>
2022-06-23 18:38:55 -0700

    assign

--- practical work/n_2/lottery_ticket.py
+++ practical work/n_2/lottery_ticket.py
@@ -5,6 +5,7 @@ k = random.randint(0, 999999)
 print(k)
 str = str(k)
 for i in range(len(str)):
-    lst.append(int(str[i]))
+    if not int(str[i]) in lst:
+        lst.append(int(str[i]))
 lst1 = sorted(lst)
 print(lst1)
