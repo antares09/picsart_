39cf36685b32f2854dc48283282bb4736b1d721f
antares <erkanyananna@mail.ru>
2022-06-23 21:21:49 -0700

    sub

--- practical work/n_2/sublist.py
+++ practical work/n_2/sublist.py
@@ -1,13 +1,11 @@
 def is_sublist(large, small):
-    sub = []
-    count = len(small)
     lst = [None] * len(small)
     c_large = []
     for i in large:
         c_large.append(i)
     if small == [] or small == large:
         return True
-    for j in range(len(large) - len(small)):
+    for j in range(len(large) - len(small)+1):
         for i in range(len(small)):
             lst[i] = c_large[i]
         if lst == small:
@@ -20,8 +18,8 @@ def is_sublist(large, small):
 lst1 = [1, 2, 3, 4, 5, 6, 7]
 lst2 = []
 lst3 = [2, 3]
-lst4 = [1, 2, 3, 4, 5, 7]
-lst5 = [1, 2, 3, 4, 5, 6, 7]
+lst4 = [1,2, 3, 4, 5, 7]
+lst5 = [2, 3, 4, 5, 6, 7]
 print(is_sublist(lst1, lst2))
 print(is_sublist(lst1, lst3))
 print(is_sublist(lst1, lst4))
