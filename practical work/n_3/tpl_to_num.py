dfeded86c13f343a4af5606daf823b78012f9209
antares <erkanyananna@mail.ru>
2022-06-25 19:54:10 -0700

    tpl

---
+++ practical work/n_3/tpl_to_num.py
@@ -0,0 +1,22 @@
+import copy
+
+
+def int_len(n):
+    count = 0
+    num = copy.copy(n)
+    while num > 0:
+        count += 1
+        num //= 10
+    return count
+
+
+def num_tuple(tpl):
+    num = 0
+    for i in range(len(tpl)):
+        num *= 10**(int_len(tpl[i]))
+        num += tpl[i]
+    return num
+
+
+tpl = (1, 2, 385, 47, 5)
+print(num_tuple(tpl))
