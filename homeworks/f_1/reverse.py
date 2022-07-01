301e2fb09a33a54dec6d3532380ce34fd03a40b7
antares <erkanyananna@mail.ru>
2022-07-01 11:52:25 -0700

    rev

--- homeworks/f_1/reverse.py
+++ homeworks/f_1/reverse.py
@@ -2,10 +2,6 @@ def reverse(data):
     ''' Rotates data in reverse order '''
     for i in range(len(data) // 2):
         data[i], data[len(data) - 1 - i] = data[len(data) - 1 - i], data[i]
-        #tmp = data[i]
-        #data[i] = data[len(data) - 1 - i]
-        #data[len(data) - 1 - i] = tmp'''
-
 
 lst = [4, 25, 1, 6, 9, 12, 36, 8, 1, 2, 1, 3]
 reverse(lst)
